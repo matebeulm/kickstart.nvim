a2229b0d90d1fb554ff12e7fcee7d17bb37e916c
Matthias Eberhardt <m.ebe@omtsys.com>
2024-06-05 07:02:51 +0200

    allow auto format of cpp files

--- lua/custom/plugins/myinit.lua
+++ lua/custom/plugins/myinit.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: missing-fields
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
@@ -135,6 +136,31 @@ return {
     },
     opts = {
       -- Your setup opts here
+    },
+  },
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true }
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use a sub-list to tell conform to run *until* a formatter
+        -- is found.
+        -- javascript = { { "prettierd", "prettier" } },
+      },
     },
   },
 }
