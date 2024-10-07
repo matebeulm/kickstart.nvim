32165f7c810287c62c053c3aeb53946dba8bab6b
Matthias Eberhardt <m.ebe@omtsys.com>
2024-10-07 11:57:45 +0200

    wip

--- init.lua
+++ init.lua
@@ -607,6 +607,7 @@ require('lazy').setup({
         pbls = {
           filetypes = { 'proto' },
         },
+        json_ls,
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
@@ -890,7 +891,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'json', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
